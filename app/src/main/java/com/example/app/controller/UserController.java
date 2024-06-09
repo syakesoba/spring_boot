0b09587ffbb9a5f7d383a132e1e3e0f1dc25d9a0
syakesoba <soccer.akira6@icloud.com>
2024-06-09 23:21:03 +0900

    react environment construction.

---
+++ app/src/main/java/com/example/app/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.app.service.UserService;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public String index(Model model) {
+
+        return "index";
+    }
+    
+}
---
+++ app/src/main/java/com/example/app/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.app.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+    
+}
